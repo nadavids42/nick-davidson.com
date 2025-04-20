cd70e9439df7709c2c1fa954d4f2b2de148e7122
nadavids42 <ndavidson42@gmail.com>
2025-04-19 20:09:59 -0400

    Add files via upload

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+});
 
+function toggleDarkMode() {
+  document.documentElement.classList.toggle('dark');
+  localStorage.setItem('theme', document.documentElement.classList.contains('dark') ? 'dark' : 'light');
+}
